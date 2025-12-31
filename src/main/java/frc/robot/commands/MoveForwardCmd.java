8da11d779526c6354223de5473139e0dd66ab93c
YuYue1025 <s1410216@my.cmsh.cyc.edu.tw>
2025-12-31 19:55:48 +0800

    修改縮排

--- src/main/java/frc/robot/commands/MoveForwardCmd.java
+++ src/main/java/frc/robot/commands/MoveForwardCmd.java
@@ -1,35 +1,36 @@
 package frc.robot.commands;
 
-import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveSubsystem;
+
 
 public class MoveForwardCmd extends Command {
 
-    DriveSubsystem driveSubsystem;
+  DriveSubsystem driveSubsystem;
     
-    public MoveForwardCmd(DriveSubsystem drive) {
-      this.driveSubsystem = drive;
-      addRequirements(drive);
-    }
+  public MoveForwardCmd(DriveSubsystem drive) {
+    this.driveSubsystem = drive;
+    addRequirements(drive);
+  }
 
-    @Override
-    public void initialize() {
+  @Override
+  public void initialize() {
       
-    }
+  }
 
-    @Override
-    public void execute() {
-        driveSubsystem.moveForward();
-    }
+  @Override
+  public void execute() {
+    driveSubsystem.moveForward();
+  }
     
-    @Override
-    public void end(boolean interrupted) {
+  @Override
+  public void end(boolean interrupted) {
       
-    }
+  }
 
-    @Override
-    public boolean isFinished() {
-      return false;
-    }
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
 
