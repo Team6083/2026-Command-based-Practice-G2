a2a754453e6187d14d06ff50637cb9581984201f
YuYue1025 <s1410216@my.cmsh.cyc.edu.tw>
2025-12-31 19:20:51 +0800

    修正encoder的角度

--- src/main/java/frc/robot/Subsystems/climberSubsystem.java
+++ src/main/java/frc/robot/Subsystems/climberSubsystem.java
@@ -6,30 +6,33 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 public class ClimberSubsystem extends SubsystemBase {
 
   private WPI_VictorSPX climberMotor = new WPI_VictorSPX(Constants.ClimberConstants.CLIMBER_MOTOR_ID);
   private DigitalInput limitSwitch = new DigitalInput(Constants.ClimberConstants.DIGITALINPUT_ID);
-  private DutyCycleEncoder encoder = new DutyCycleEncoder(Constants.ClimberConstants.DUTYCYCLEENCODER_ID, Constants.ClimberConstants.DUTYCYCLEENCODER_FULLRANGE,  Constants.ClimberConstants.DUTYCYCLEENCODER_EXPECTEDZERO);  
+  private Encoder encoder = new Encoder(8, 9);
   private PIDController pidDown = new PIDController(Constants.ClimberConstants.PIDCONTROLLER_DOWN_KP, Constants.ClimberConstants.PIDCONTROLLER_DOWN_KI, Constants.ClimberConstants.PIDCONTROLLER_DOWN_KD);
   private PIDController pidUp = new PIDController(Constants.ClimberConstants.PIDCONTROLLER_UP_KP, Constants.ClimberConstants.PIDCONTROLLER_UP_KI, Constants.ClimberConstants.PIDCONTROLLER_UP_KD);
   private double setpoint;
   /** Creates a new climberSubsystem. */
   public ClimberSubsystem() {
+    encoder.setDistancePerPulse(360.0 / 2048.0);
     pidUp.enableContinuousInput(0,360);
     pidDown.enableContinuousInput(0,360);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("encoder", encoder.get());
+    SmartDashboard.putNumber("encoder", encoder.getDistance());
   }
   public void reset(){
     pidUp.reset();
@@ -38,8 +41,8 @@ public class ClimberSubsystem extends SubsystemBase {
   
   public void deepCageDown(){
     setpoint = 90.0;
-    double currentPosition = encoder.get();
-    double output = -(pidDown.calculate(currentPosition, setpoint));
+    double currentPosition = encoder.getDistance();
+    double output = pidDown.calculate(currentPosition, setpoint);
     climberMotor.set(ControlMode.PercentOutput, MathUtil.clamp(output, -0.2, 0.2));
   }
 
@@ -52,7 +55,7 @@ public class ClimberSubsystem extends SubsystemBase {
     } 
 
     setpoint = 0.0;
-    double currentPosition = encoder.get();
+    double currentPosition = encoder.getDistance();
     double output = -(pidUp.calculate(currentPosition, setpoint));
     climberMotor.set(ControlMode.PercentOutput, MathUtil.clamp(output, -0.2, 0.2));
   }
